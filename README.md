f37c3e8b4553634a3a7b2494fdf890bb4d38160b
Alexander Morales <klank4135@gmail.com>
2019-03-14 19:23:02 -0600

    Initial setup of the mon_intro.sh file, adding some readme instructions

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # mon_intro
 Monitoring Basics Meetup
+
+```bash
+#start the project with default configuration
+~#> ./mon_intro start
+
+#stop the project with default configuration
+~#> ./mon_intro stop
+```
---
+++ mon_intro.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+echo "##### Welcome to mon_intro project #####"
+
+function start_docker_elk(){
+echo "Downloading the Docker projects"
+delk="docker-elk"
+	if [ -d "$delk" ]
+	then
+		echo "$delk repository found."
+		echo "Building and starting docker-elk stack"
+	else
+		echo "Cloning the repository https://github.com/deviantony/docker-elk.git"
+		git clone https://github.com/deviantony/docker-elk.git
+	fi
+
+cd docker-elk && docker-compose up -d
+}
+
+function stop_docker_elk(){
+echo "Stopping the Docker ELK project"
+cd docker-elk && docker-compose stop
+}
+
+
+if [ "$@" == "start" ]
+	then
+    start_docker_elk
+elif [ "$@" == "stop" ]
+	then
+	stop_docker_elk
+fi
