b299e63fd59487258f9c3920c2d23af93759cbaa
Alexander Morales <alexmorales@paypal.com>
2019-04-12 10:56:56 -0700

    Clean Up of unused clones on mon_intro.sh script

--- mon_intro.sh
+++ mon_intro.sh
@@ -3,51 +3,35 @@ workdir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 echo "##### Welcome to mon_intro project #####"
 
 function start_docker_elk(){
-echo "Downloading the Docker projects"
-delk="docker-elk"
-	if [ -d "$delk" ]; then
-		echo "$delk repository found."
-		echo "Building and starting docker-elk stack"
-	else
-		echo "Cloning the repository https://github.com/deviantony/docker-elk.git"
-		git clone https://github.com/deviantony/docker-elk.git $workdir/docker-elk
-	fi
-
-cd $workdir/docker-elk && docker-compose up -d
+	echo "Starting ELK Stack"
+	cd $workdir/docker-elk && docker-compose up -d
 }
 
 function start_mon_talk(){
-mtalk="mon-talk"
-	if [ -d "$mtalk" ]; then
-		echo "$mtalk repository found"
-		echo "Building and starting Prometheous and Grafana Containers"
-	else
-		echo "Cloning the repository https://github.com/klank4135/mon-talk.git"
-		git clone https://github.com/klank4135/mon-talk.git $workdir/mon-talk
-	fi
-cd $workdir/mon-talk && docker-compose up -d
+	echo "Starting Metric apps (Grafana/Prometheous)"
+	cd $workdir/mon-talk && docker-compose up -d
 }
 
 function start_jaeger(){
-echo "Starting Jaeger Container"
-docker run -d --name jaeger \
-	-e COLLECTOR_ZIPKIN_HTTP_PORT=9411 \
-  	-p 5775:5775/udp \
-  	-p 6831:6831/udp \
-  	-p 6832:6832/udp \
-  	-p 5778:5778 \
-  	-p 16686:16686 \
-  	-p 14268:14268 \
-  	-p 9411:9411 \
-  	jaegertracing/all-in-one:1.8
+	echo "Starting Jaeger Container"
+	docker run -d --name jaeger \
+		-e COLLECTOR_ZIPKIN_HTTP_PORT=9411 \
+			-p 5775:5775/udp \
+			-p 6831:6831/udp \
+			-p 6832:6832/udp \
+			-p 5778:5778 \
+			-p 16686:16686 \
+			-p 14268:14268 \
+			-p 9411:9411 \
+			jaegertracing/all-in-one:1.8
 
-echo "Starting Demo Application"
-docker run -d --name jaeger_hotrod \
-  --link jaeger \
-  -p8880-8883:8080-8083 \
-  -e JAEGER_AGENT_HOST="jaeger" \
-  jaegertracing/example-hotrod:1.8 \
-  all
+	echo "Starting Demo Application"
+	docker run -d --name jaeger_hotrod \
+		--link jaeger \
+		-p8880-8883:8080-8083 \
+		-e JAEGER_AGENT_HOST="jaeger" \
+		jaegertracing/example-hotrod:1.8 \
+		all
 }
 
 function setup_logs(){
@@ -72,27 +56,21 @@ function setup_logs(){
 }
 
 function stop_docker_elk(){
-
-echo "Stop and remove Docker ELK projects"
-cd $workdir/docker-elk && docker-compose stop
-
+	echo "Stop and remove Docker ELK projects"
+	cd $workdir/docker-elk && docker-compose stop
 }
 
 function stop_mon_talk(){
-
-echo "Stop and remove Docker Mon-Intro projects"
-cd $workdir/mon-talk && docker-compose stop
-
+	echo "Stop and remove Docker Mon-Intro projects"
+	cd $workdir/mon-talk && docker-compose stop
 }
 
 function stop_jaeger(){
-
-echo "Stop and remove Jaeger Docker Containers"
-
-docker stop jaeger
-docker rm -f jaeger
-docker stop jaeger_hotrod
-docker rm -f jaeger_hotrod
+	echo "Stop and remove Jaeger Docker Containers"
+	docker stop jaeger
+	docker rm -f jaeger
+	docker stop jaeger_hotrod
+	docker rm -f jaeger_hotrod
 }
 
 if [ "$@" == "start" ]; then
